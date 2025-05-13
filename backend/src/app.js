df845aeb337378932efa4fef7aa64b271ec8ee30
zearanha <JAranha21@gmail.com>
2025-05-13 02:01:35 -0300

    Variavel de ambiente

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv'
 
 import mongoRoutes from "./routes/mongoRoutes.js";
 import prismaRoutes from "./routes/mysqlRoutes.js";
 
+dotenv.config();
+
+var port = process.env.PORT;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+console.log(process.env.MONGO_URI)
 
 mongoose
   .connect(
-    "mongodb+srv://aranhafalcao10:mScHaWCsfmJXldw4@listtarefs.wav2rts.mongodb.net/ListTarefs?retryWrites=true&w=majority&appName=ListTarefs"
+    process.env.MONGO_URI 
   )
   .then(() => {
     console.log("banco de dados conectado");
@@ -25,6 +30,6 @@ app.use('/listTarefs', mongoRoutes);
 app.use('/addUser', prismaRoutes);
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Servidor rodando na porta 3000');
 })
