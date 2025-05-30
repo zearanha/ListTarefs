db0b04fb9f4562d4bdbf570386fc3d77a0ac2ca2
zearanha <JAranha21@gmail.com>
2025-05-30 02:46:46 -0300

    Trazendo usuarios não logados para a pagina de Login

--- frontend/listtarefs/src/app/user/tarefas/page.tsx
+++ frontend/listtarefs/src/app/user/tarefas/page.tsx
@@ -1,6 +1,11 @@
 'use client'
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation";
+import { jwtDecode } from "jwt-decode";
+
+type JwtPayLoad = {
+    exp: number;
+}
 
 export default function Tarefs(){
     const router = useRouter();
@@ -8,16 +13,40 @@ export default function Tarefs(){
 
     useEffect(() => {
         const token = localStorage.getItem('token');
+        console.log('Token:', token);
+        // Check if the token exists
         if(!token){
             router.push('/user')
-        } else {
-            setAuthorized(true);
+            return;
+        } 
+
+        try{
+            const decoded = jwtDecode<JwtPayLoad>(token);
+            const now = Date.now() / 1000;
+            if(decoded.exp < now){
+                localStorage.removeItem('token');
+                router.push('/user');
+                return;
+            }
+            else{
+                setAuthorized(true);
+                const msToExpire = (decoded.exp - now) * 1000;
+                const timeout = setTimeout(() => {
+                    localStorage.removeItem('token');
+                    router.push('/user');
+                }, msToExpire)
+                return () => clearTimeout(timeout);
+            }
+        }
+        catch{
+            localStorage.removeItem('token');
+            router.push('/user')
         }
     }, [router])
 
     if(!authorized) return null;
 
     return(
-        <> <h1>Hello</h1></>
+        <> <h1>Acesso permitido</h1></>
     )
 }
